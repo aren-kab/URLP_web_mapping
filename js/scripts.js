77730a32eb624f0fe5e011950d8690d0a2a62fb0
aren-kab <akabarajian@gmail.com>
2021-02-04 19:39:11 -0500

    update_site_1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-console.log('Hello, looks like the js file is loading properly.')
+//on click
+$('.grid-item').on('click', function() {
+	// pull out the album cover url from the element's attributes
+	var foodUrl = $(this).attr('food')
+  // log the album cover url to the console
+  console.log(foodUrl)
+
+  $('#food-art').empty()
+
+  $('#food-art').css('background-image', `url(${foodUrl})`)
+})
+
+$("#SPA").mouseover(function(){
+  $(".description").text("Jamon - Cured Iberian Ham");
+});
+
+$("#ARM").mouseover(function(){
+  $(".description").text("Boreg - Cheese & Philadough Pastry");
+});
+
+$("#ITA").mouseover(function(){
+  $(".description").text("Zuchini Fries - Zuchini cut in small sticks and fried");
+  $
+});
+
+$("#JPA").mouseover(function(){
+  $(".description").text("Dumplings - Pork & Chives wrapped in pan fried dough");
+});
